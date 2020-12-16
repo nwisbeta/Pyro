f1eafae49a3f79e85bb0c24ab1d729f5a95b1773
bmcd77 <bmcd77@hotmail.com>
2020-12-16 17:11:33 +0000

    Add Validation for processMessage bundle for a Pathology Report

--- Pyro.Common/FhirOperation/ProcessMessage/ProcessMessageOperation.cs
+++ Pyro.Common/FhirOperation/ProcessMessage/ProcessMessageOperation.cs
@@ -41,16 +41,60 @@ namespace Pyro.Common.FhirOperation.ProcessMessage
             {
                 IssueList.Add(Pyro.Common.Tools.FhirOperationOutcomeSupport.CreateIssue(OperationOutcome.IssueSeverity.Error, OperationOutcome.IssueType.Invalid, $"The ${_OperationName} can only accept Bundle resources of Bundle.type = 'message'. Type found was {bundle.Type.ToString()}"));
             }
-            else
+            else if (bundle.Entry.FirstOrDefault()?.Resource is MessageHeader)
             {
-                //TODO: Process the message bundle
-                ResourceServiceOutcome.HttpStatusCode = System.Net.HttpStatusCode.OK;
-                //ResourceServiceOutcome.ResourceResult = ProcessMessageResult;
-                ResourceServiceOutcome.OperationType = Enum.RestEnum.CrudOperationType.Update;
-                ResourceServiceOutcome.SuccessfulTransaction = true;
+                var messageHeader = bundle.Entry.First().Resource as MessageHeader;
+                var orgResourceRef = messageHeader.Sender;
+                var coding = messageHeader.Event as Coding;
+                if (coding == null)
+                {
+                    IssueList.Add(Tools.FhirOperationOutcomeSupport.CreateIssue(OperationOutcome.IssueSeverity.Error, OperationOutcome.IssueType.Invalid, $"The ${_OperationName} has a MessageHeader but an eventCoding was missing or invalid. {bundle.Type.ToString()}"));
+                }
+                else if (!coding.System.Contains("http://snomed.info/sct") || !coding.Code.Contains("371528001"))
+                {
+                    if (!coding.System.Contains("http://snomed.info/sct"))
+                        IssueList.Add(Tools.FhirOperationOutcomeSupport.CreateIssue(OperationOutcome.IssueSeverity.Error, OperationOutcome.IssueType.Invalid, $"The ${_OperationName} expects a valid eventCoding snomed code or the code you submitted is not supported. {bundle.Type.ToString()}"));
+                    else
+                        IssueList.Add(Tools.FhirOperationOutcomeSupport.CreateIssue(OperationOutcome.IssueSeverity.Error, OperationOutcome.IssueType.Invalid, $"The ${_OperationName} expects a valid supported eventCode containing a supported snomed reference to snomed.info/sct. {bundle.Type.ToString()}"));
+                }
+                // Organization check : This is only checking the Sender matches the ref (not that it's an organisation)
+                else if (!bundle.FindEntry(orgResourceRef).Any())
+                {
+                    IssueList.Add(Tools.FhirOperationOutcomeSupport.CreateIssue(OperationOutcome.IssueSeverity.Error, OperationOutcome.IssueType.Invalid, $"The ${_OperationName} expects a valid Organization but could not find one. {bundle.Type.ToString()}"));
+                }
+                else if (!IssueList.Any())  // If we have not issues at this point check we have an org and diagnostic report
+                {
+                    // We must have an Organization and a DiagnosticReport in the bundle
+                    var orgFound = false;
+                    var diagnosticReportFound = false;
+                    foreach (var entry in bundle.Entry)
+                    {
+                        switch (entry.Resource)
+                        {
+                            case Organization _:
+                                orgFound = true;
+                                break;
+                            case DiagnosticReport _:
+                                diagnosticReportFound = true;
+                                break;
+                        }
+                    }
 
+                    if (!orgFound)
+                        IssueList.Add(Tools.FhirOperationOutcomeSupport.CreateIssue(OperationOutcome.IssueSeverity.Error, OperationOutcome.IssueType.Invalid, $"The ${_OperationName} expects a valid Organization but could not find one. {bundle.Type.ToString()}"));
+                    if (!diagnosticReportFound)
+                        IssueList.Add(Tools.FhirOperationOutcomeSupport.CreateIssue(OperationOutcome.IssueSeverity.Error, OperationOutcome.IssueType.Invalid, $"The ${_OperationName} expects a valid DiagnosticReport but could not find one. {bundle.Type.ToString()}"));
+                }
             }
+            else
+                IssueList.Add(Pyro.Common.Tools.FhirOperationOutcomeSupport.CreateIssue(OperationOutcome.IssueSeverity.Error, OperationOutcome.IssueType.Invalid, $"The ${_OperationName} has found a Bundle but expects a resourceType of Messageheader as the first entry"));
 
+            //TODO: Process the message bundle
+            ResourceServiceOutcome.HttpStatusCode = System.Net.HttpStatusCode.OK;
+            //ResourceServiceOutcome.ResourceResult = ProcessMessageResult;
+            ResourceServiceOutcome.OperationType = Enum.RestEnum.CrudOperationType.Update;
+            ResourceServiceOutcome.SuccessfulTransaction = true;
+            
             if (IssueList.Count > 0)
             {
                 var Op = Common.Tools.FhirOperationOutcomeSupport.Generate(IssueList);
