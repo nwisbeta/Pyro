d4169ea79b30b7a18fb2ddd3b4b86445e5d2e583
velsietis <huw.angle@wales.nhs.uk>
2020-12-17 18:21:58 +0000

    Update validation logic for pathology request message

--- Pyro.Common/FhirOperation/ProcessMessage/ProcessMessageOperation.cs
+++ Pyro.Common/FhirOperation/ProcessMessage/ProcessMessageOperation.cs
@@ -1,4 +1,5 @@
 ﻿using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
 using Pyro.Common.CompositionRoot;
 using Pyro.Common.Enum;
 using Pyro.Common.Interfaces.Service;
@@ -17,9 +18,8 @@ namespace Pyro.Common.FhirOperation.ProcessMessage
     {
         private readonly IResourceServiceOutcomeFactory IResourceServiceOutcomeFactory;
 
-        //private string _OperationName = FhirOperationEnum.OperationType.ProcessMessage.GetPyroLiteral();
-        private string _OperationName = "process-message";
-
+        private string _OperationName = FhirOperationEnum.OperationType.ProcessMessage.GetPyroLiteral();
+        
         public ProcessMessageOperation(IResourceServices IResourceServices, IResourceServiceOutcomeFactory IResourceServiceOutcomeFactory, ISearchParameterServiceFactory ISearchParameterServiceFactory, IRequestMetaFactory IRequestMetaFactory)
         {
             this.IResourceServiceOutcomeFactory = IResourceServiceOutcomeFactory;
@@ -41,53 +41,22 @@ namespace Pyro.Common.FhirOperation.ProcessMessage
             {
                 IssueList.Add(Pyro.Common.Tools.FhirOperationOutcomeSupport.CreateIssue(OperationOutcome.IssueSeverity.Error, OperationOutcome.IssueType.Invalid, $"The ${_OperationName} can only accept Bundle resources of Bundle.type = 'message'. Type found was {bundle.Type.ToString()}"));
             }
-            else if (bundle.Entry.FirstOrDefault()?.Resource is MessageHeader)
+            else if (! (bundle.Entry.FirstOrDefault()?.Resource is MessageHeader))
             {
-                var messageHeader = bundle.Entry.First().Resource as MessageHeader;
-                var orgResourceRef = messageHeader.Sender;
-                var coding = messageHeader.Event as Coding;
-                if (coding == null)
-                {
-                    IssueList.Add(Tools.FhirOperationOutcomeSupport.CreateIssue(OperationOutcome.IssueSeverity.Error, OperationOutcome.IssueType.Invalid, $"The ${_OperationName} has a MessageHeader but an eventCoding was missing or invalid. {bundle.Type.ToString()}"));
-                }
-                else if (!coding.System.Contains("http://snomed.info/sct") || !coding.Code.Contains("371528001"))
-                {
-                    if (!coding.System.Contains("http://snomed.info/sct"))
-                        IssueList.Add(Tools.FhirOperationOutcomeSupport.CreateIssue(OperationOutcome.IssueSeverity.Error, OperationOutcome.IssueType.Invalid, $"The ${_OperationName} expects a valid eventCoding snomed code or the code you submitted is not supported. {bundle.Type.ToString()}"));
-                    else
-                        IssueList.Add(Tools.FhirOperationOutcomeSupport.CreateIssue(OperationOutcome.IssueSeverity.Error, OperationOutcome.IssueType.Invalid, $"The ${_OperationName} expects a valid supported eventCode containing a supported snomed reference to snomed.info/sct. {bundle.Type.ToString()}"));
-                }
-                // Organization check : This is only checking the Sender matches the ref (not that it's an organisation)
-                else if (!bundle.FindEntry(orgResourceRef).Any())
-                {
-                    IssueList.Add(Tools.FhirOperationOutcomeSupport.CreateIssue(OperationOutcome.IssueSeverity.Error, OperationOutcome.IssueType.Invalid, $"The ${_OperationName} expects a valid Organization but could not find one. {bundle.Type.ToString()}"));
-                }
-                else if (!IssueList.Any())  // If we have not issues at this point check we have an org and diagnostic report
-                {
-                    // We must have an Organization and a DiagnosticReport in the bundle
-                    var orgFound = false;
-                    var diagnosticReportFound = false;
-                    foreach (var entry in bundle.Entry)
-                    {
-                        switch (entry.Resource)
-                        {
-                            case Organization _:
-                                orgFound = true;
-                                break;
-                            case DiagnosticReport _:
-                                diagnosticReportFound = true;
-                                break;
-                        }
-                    }
-
-                    if (!orgFound)
-                        IssueList.Add(Tools.FhirOperationOutcomeSupport.CreateIssue(OperationOutcome.IssueSeverity.Error, OperationOutcome.IssueType.Invalid, $"The ${_OperationName} expects a valid Organization but could not find one. {bundle.Type.ToString()}"));
-                    if (!diagnosticReportFound)
-                        IssueList.Add(Tools.FhirOperationOutcomeSupport.CreateIssue(OperationOutcome.IssueSeverity.Error, OperationOutcome.IssueType.Invalid, $"The ${_OperationName} expects a valid DiagnosticReport but could not find one. {bundle.Type.ToString()}"));
-                }
+                IssueList.Add(Pyro.Common.Tools.FhirOperationOutcomeSupport.CreateIssue(OperationOutcome.IssueSeverity.Error, OperationOutcome.IssueType.Invalid, $"Instance failed constraint bdl-12 \"A message must have a MessageHeader as the first resource\""));
+               
             }
             else
-                IssueList.Add(Pyro.Common.Tools.FhirOperationOutcomeSupport.CreateIssue(OperationOutcome.IssueSeverity.Error, OperationOutcome.IssueType.Invalid, $"The ${_OperationName} has found a Bundle but expects a resourceType of Messageheader as the first entry"));
+            {
+                try {
+                    ProcessMessage(bundle, IssueList);
+                }
+                catch(FhirOperationException fhirEx)
+                {
+                    IssueList.Add(Pyro.Common.Tools.FhirOperationOutcomeSupport.CreateIssue(OperationOutcome.IssueSeverity.Error, OperationOutcome.IssueType.Exception, fhirEx.Message));
+                }
+               
+            }
 
             //TODO: Process the message bundle
             ResourceServiceOutcome.HttpStatusCode = System.Net.HttpStatusCode.OK;
@@ -110,9 +79,119 @@ namespace Pyro.Common.FhirOperation.ProcessMessage
             }
         }
 
-        public void ProcessPathologyTest()
+        public void ProcessMessage(Bundle bundle, List<OperationOutcome.IssueComponent> IssueList)
         {
+            var messageHeader = bundle.Entry.First().Resource as MessageHeader;
 
+            if (!(messageHeader.Event is Coding coding))
+            {
+                IssueList.Add(InvalidIssue("MessageHeader.eventCoding is missing"));
+
+            }
+            else if (coding.System == CodeSystemUrl.Snomed && coding.Code == PathologyTestCode)
+            {
+                ProcessPathologyRequest(bundle, IssueList);
+            }
+            else
+            {
+                IssueList.Add(InvalidIssue($"Message event \"${coding.System}|${coding.Code}\" is not supported."));
+            }
         }
+
+        public void ProcessPathologyRequest(Bundle bundle, List<OperationOutcome.IssueComponent> IssueList)
+        {
+            var messageHeader = bundle.Entry.First().Resource as MessageHeader;
+
+            // Validate "Pathology Request" bundle
+            if (!(TryFind(bundle, entryRef: messageHeader.Sender) is Organization))
+            {
+                IssueList.Add(InvalidIssue($"MessageHeader.Sender does not reference an Organization resource in the bundle"));
+            }
+
+            // Diagnostic Report exists?
+            if (TryFind(bundle, entryRef: messageHeader.Focus?.FirstOrDefault()) is DiagnosticReport report)
+            {
+                //Observation exists?
+                if (TryFind(bundle, entryRef: report.Result?.FirstOrDefault()) is Observation observation)
+                {
+                    // Validate Observation
+                    if (!observation.Issued.HasValue)
+                    {
+                        IssueList.Add(InvalidIssue($"Observation.Issue is missing"));
+                    }
+                    if (observation.Code?.Coding?.FirstOrDefault()?.System != CodeSystemUrl.Snomed)
+                    {
+                        IssueList.Add(InvalidIssue($"Observation.Code.Coding.System must be \"${CodeSystemUrl.Snomed}\""));
+                    }
+
+                    if (TryFind(bundle, entryRef: observation.Subject) is Patient patient)
+                    {
+                        // Validate Patient
+                        var hasName = patient.Name.Any(name => name.Given.Any() && name.Family.Any());
+                        var hasDob = patient.BirthDate != null;
+                        var hasPostalcode = patient.Address.Any(address => address.PostalCode != null);
+
+                        if (!(hasName && hasDob && hasPostalcode))
+                        {
+                            IssueList.Add(InvalidIssue($"Patient does not include required data: given name, family name, date of birth and postal code"));
+                        }
+                    }
+                    else
+                    {
+                        IssueList.Add(InvalidIssue($"Observation.Subject does not reference an Patient resource in the bundle"));
+                    }
+
+                    if (!(TryFind(bundle, entryRef: observation.Encounter) is Encounter))
+                    {
+                        IssueList.Add(InvalidIssue($"Observation.Encounter does not reference an Encounter resource in the bundle"));
+                    }
+                    if (!(TryFind(bundle, entryRef: observation.Device) is Device))
+                    {
+                        IssueList.Add(InvalidIssue($"Observation.Device does not reference an Device resource in the bundle"));
+                    }
+                    if (!(TryFind(bundle, entryRef: observation.Specimen) is Specimen))
+                    {
+                        IssueList.Add(InvalidIssue($"Observation.Specimen does not reference an Specimen resource in the bundle"));
+                    }
+                    if (TryFind(bundle, entryRef: observation.BasedOn.FirstOrDefault()) is Media media
+                        && !(TryFind(bundle, entryRef: media.Device) is Device))
+                    {
+                        IssueList.Add(InvalidIssue($"Media.Device does not reference an Device resource in the bundle"));
+                    }                   
+                }
+                else
+                {
+                    IssueList.Add(InvalidIssue($"DiagnosticReport.Result does not reference an Observation resource in the bundle"));
+                }
+            }
+            else
+            {
+                IssueList.Add(InvalidIssue($"MessageHeader.Focus does not reference a DiagnosticReport resource in the bundle"));
+            }
+        }
+
+
+
+
+
+        const string PathologyTestCode = "371528001";
+        private static class CodeSystemUrl
+        {
+            public static string Snomed => "http://snomed.info/sct";
+        }
+
+        private OperationOutcome.IssueComponent InvalidIssue(string message)
+        {
+            return Tools.FhirOperationOutcomeSupport.CreateIssue(OperationOutcome.IssueSeverity.Error, OperationOutcome.IssueType.Invalid, message);
+        }
+
+        private Resource TryFind(Bundle bundle, ResourceReference entryRef)
+        {
+            if (entryRef is null)
+                return null;
+
+            return bundle.FindEntry(entryRef).FirstOrDefault()?.Resource;
+        }
+
     }
 }
